aa34f78aa9651e7850c09da6a4a5e53879fda582
关振兴 <guan.zhenxing@foxmail.com>
2017-01-25 16:12:44 +0800

    routes方法重新定义

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router'
-import routes from '../routes'
+import routes from '../routes/index'
 import DevTools from './DevTools'
 import GeneralUtil from '../utils/GeneralUtil'
 
