c0235bb527a90c3d789dff860e58cc307114b582
关振兴 <guan.zhenxing@foxmail.com>
2017-01-25 15:39:16 +0800

    整合configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,69 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
+
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import api from '../middlewares/api'
+import apiRequester from '../middlewares/apiRequester'
+import rootReducer from '../reducers'
+
+import createLogger from 'redux-logger'
+import DevTools from '../containers/DevTools'
+
+const isProdEnv = () => {
+    return process.env.NODE_ENV === 'production'
 }
+
+/**
+ * store增强
+ * @param node_env
+ * @returns {*}
+ */
+const storeEnhancer = () => {
+    if (isProdEnv()) {  //生产环境配置
+        return applyMiddleware(thunk, api, apiRequester)
+    } else {    //开发环境配置
+        return compose(
+            applyMiddleware(thunk, api, apiRequester, createLogger()),
+            DevTools.instrument()
+        )
+    }
+};
+
+/**
+ * 给开发环境中的webpack替换掉reducers使用
+ * @param store
+ */
+const webpackHotReplaceReducers = store => {
+
+    if (!isProdEnv()) {  //开发环境
+        if (module.hot) {
+            // Enable Webpack hot module replacement for reducers
+            module.hot.accept('../reducers', () => {
+                const nextRootReducer = require('../reducers').default;
+                store.replaceReducer(nextRootReducer)
+            })
+        }
+    }
+
+};
+
+/**
+ * 配置store
+ * @param preLoadedState
+ * @returns {Store<S>}
+ */
+const configureStore = preLoadedState => {
+
+    const store = createStore(
+        rootReducer,
+        preLoadedState,
+        storeEnhancer()
+    );
+
+    webpackHotReplaceReducers(store);
+
+    return store
+};
+
+export default configureStore
+
+
