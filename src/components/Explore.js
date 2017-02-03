356de973c18e571fb4f51a32ceea7b699949c502
关振兴 <guan.zhenxing@foxmail.com>
2017-02-03 11:48:34 +0800

    模块化CSS

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 import {Input, Button} from 'antd';
-import './Explore.css';
+import styles from './Explore.css';
 const GITHUB_REPO = 'https://github.com/reactjs/redux'
 
 export default class Explore extends Component {
@@ -37,7 +37,7 @@ export default class Explore extends Component {
         return (
             <div>
                 <p>Type a username or repo full name and hit 'Go':</p>
-                <div className="search">
+                <div className={styles['search']}>
                     <Input ref="input" placeholder="Basic usage"
                            defaultValue={this.props.value}
                            onKeyUp={this.handleKeyUp}/>
