89b31419300b4a2c28f073df685ec9949b61fcaf
关振兴 <guan.zhenxing@foxmail.com>
2017-02-24 16:32:22 +0800

    定义reducer、action等

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,15 +1,5 @@
 /**
  * Created by guanzhenxing on 2017-02-09.
  */
-import {handleActions} from 'redux-actions'
-export default handleActions({
-    FETCH_DATA: (state, action) => {
-        console.log(action)
-        return {
-            ...state,
-            ...action.payload
-        }
-    }
-}, {
-    data: ''
-})
+
+import {fromJS} from 'immutable';
